dd65f2a3eacfcaa2260c21f6fa4df92ff5cabe0c
Rashidov Rashidov <rashid.rashidov@sap.com>
2020-08-26 21:43:55 +0300

    Introduce fic module

---
+++ org.roko.ns3.fic/src/test/java/org/roko/ns3/org/roko/ns3/fic/FileIdCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roko.ns3.org.roko.ns3.fic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileIdCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
