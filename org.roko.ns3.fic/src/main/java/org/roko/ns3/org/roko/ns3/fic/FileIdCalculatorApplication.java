75f9511b562d0fa2f977e68e10b555d6a685c06b
Rashidov Rashidov <rashid.rashidov@sap.com>
2020-08-26 21:43:55 +0300

    Introduce fic module

---
+++ org.roko.ns3.fic/src/main/java/org/roko/ns3/org/roko/ns3/fic/FileIdCalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.roko.ns3.org.roko.ns3.fic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIdCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileIdCalculatorApplication.class, args);
+	}
+
+}
