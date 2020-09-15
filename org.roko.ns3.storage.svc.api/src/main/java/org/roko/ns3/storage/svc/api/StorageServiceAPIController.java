f4e3dd424a3b30dcfd7e0f62d1372b8c19d89903
Rashidov Rashidov <rashid.rashidov@sap.com>
2020-09-15 11:50:10 +0300

    Introduce storage svc api

---
+++ org.roko.ns3.storage.svc.api/src/main/java/org/roko/ns3/storage/svc/api/StorageServiceAPIController.java
@@ -0,0 +1,34 @@
+package org.roko.ns3.storage.svc.api;
+
+import java.io.IOException;
+
+import org.roko.ns3.fic.client.api.FileIDCalculatorClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/files")
+public class StorageServiceAPIController {
+
+	private FileIDCalculatorClient fileIdCalculatorClient;
+
+	@Autowired
+	public StorageServiceAPIController(FileIDCalculatorClient fileIdCalculatorClient) {
+		this.fileIdCalculatorClient = fileIdCalculatorClient;
+	}
+
+	@GetMapping
+	public String get() {
+		return "Hello World!";
+	}
+	
+	@PostMapping
+	public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+		return fileIdCalculatorClient.calculate(file.getBytes());
+	}
+}
---
+++ org.roko.ns3.storage.svc.api/src/test/java/org/roko/ns3/storage/svc/api/StorageServiceAPIControllerTest.java
@@ -0,0 +1,48 @@
+package org.roko.ns3.storage.svc.api;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.roko.ns3.fic.client.api.FileIDCalculatorClient;
+import org.springframework.web.multipart.MultipartFile;
+
+public class StorageServiceAPIControllerTest {
+
+	private static final String TEST_FILE_ID = "123123";
+
+	private static final byte[] TEST_BYTE_ARRAY = new byte[] {};
+
+	@Mock
+	private FileIDCalculatorClient fileIdCalculatorClientMock;
+
+	@Mock
+	private MultipartFile fileMock;
+
+	private StorageServiceAPIController controller;
+
+	@BeforeEach
+	public void setup() throws IOException {
+		MockitoAnnotations.initMocks(this);
+
+		when(fileMock.getBytes()).thenReturn(TEST_BYTE_ARRAY);
+		
+		when(fileIdCalculatorClientMock.calculate(any(byte[].class))).thenReturn(TEST_FILE_ID);
+
+		controller = new StorageServiceAPIController(fileIdCalculatorClientMock);
+	}
+
+	@Test
+	public void fileIdIsCalculatedWhenFileIsStored() throws IOException {
+		controller.upload(fileMock);
+		
+		verify(fileIdCalculatorClientMock).calculate(TEST_BYTE_ARRAY);
+	}
+}
