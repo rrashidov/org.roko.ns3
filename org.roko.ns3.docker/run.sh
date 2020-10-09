c459600fe5885250e07e1455eaa01a34572e3f36
Rashid Rashidov <rrashidov@gmail.com>
2020-10-09 12:11:16 +0300

    [docker] Add catalog service and db to docker-compose (#49)

--- org.roko.ns3.docker/run.sh
+++ org.roko.ns3.docker/run.sh
@@ -5,12 +5,12 @@
 cp ../org.roko.ns3.fic.api/target/org.roko.ns3.fic.api-0.0.1-SNAPSHOT.jar ./docker_context/
 cp ../org.roko.ns3.storage.bucket.api/target/org.roko.ns3.storage.bucket.api-0.0.1-SNAPSHOT.jar ./docker_context/
 cp ../org.roko.ns3.storage.api/target/org.roko.ns3.storage.api-0.0.1-SNAPSHOT.jar ./docker_context/
+cp ../org.roko.ns3.catalog.api/target/org.roko.ns3.catalog.api-0.0.1-SNAPSHOT.jar ./docker_context/
 
 cp Dockerfile-fic ./docker_context/
 cp Dockerfile-storage-bucket ./docker_context/
 cp Dockerfile-storage ./docker_context/
-
-cp storage_bucket_client_config.json ./docker_context/
+cp Dockerfile-catalog ./docker_context/
 
 cp storage_bucket_client_config.json ./docker_context/
 
